2892d91ce90c52e421db626aa52fbdc34547c3a9
Ramsharan Kanwar <ryonistic@protonmail.com>
2022-04-14 14:34:27 +0530

    fixed media url

--- config/settings.py
+++ config/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 # LOGIN_URL = 'login'
 # LOGIN_REDIRECT_URL = 'home'
 # LOGOUT_URL = 'logout'
