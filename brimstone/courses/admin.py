96c01bb5c1b945711c7519d05bcb34239101f3f0
ryonistic@protonmail.com <ryonistic@protonmail.com>
2022-04-20 14:48:02 +0530

    Added subjects, lessons, rooms, etc.

--- brimstone/courses/admin.py
+++ brimstone/courses/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Course
+from .models import *
 
 
 admin.site.register(Course)
+admin.site.register(Lesson)
+admin.site.register(Subject)
+admin.site.register(Room)
+admin.site.register(DayTime)
--- brimstone/courses/models.py
+++ brimstone/courses/models.py
@@ -1,7 +1,7 @@
-"""This area is reserved for defining object\
-        instances for the database tables"""
+"""This area is reserved for defining object instances for the database tables"""
 from django.db import models
 from django.conf import settings
+from django.db.models.enums import Choices
 
 class Course(models.Model):
     """ This is a model for adding courses in the database"""
@@ -18,4 +18,45 @@ class Course(models.Model):
     stream = models.CharField(choices = STREAM_CHOICES, max_length=55)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=30)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return str(self.name)
+
+class Room(models.Model):
+    number = models.PositiveIntegerField()
+
+    def __str__(self):
+        return str(self.number)
+
+class DayTime(models.Model):
+    DAY_CHOICES = (
+            ('1','Monday'),
+            ('2','Tuesday'),
+            ('3','Wednesday'),
+            ('4','Thursday'),
+            ('5','Friday'),
+            ('6','Saturday'),
+            ('7','Sunday'),
+            )
+
+    day = models.CharField(choices = DAY_CHOICES, max_length=20)
+    time = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return str(self.time) + "'o clock on " + str(self.DAY_CHOICES[int(self.day)-1][1])
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=100)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, null=True, on_delete=models.SET_NULL)
+    time = models.ForeignKey(DayTime, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return str(self.name)
