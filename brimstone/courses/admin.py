a9f96a11fefa20018014668217289582617bf959
ryonistic@protonmail.com <ryonistic@protonmail.com>
2022-04-21 12:53:24 +0530

    Tinkering with homepage and dashboard

--- brimstone/courses/admin.py
+++ brimstone/courses/admin.py
@@ -4,6 +4,10 @@ from .models import Course, Lesson, Subject, Room, DayTime
 
 admin.site.register(Course)
 admin.site.register(Lesson)
-admin.site.register(Subject)
+# admin.site.register(Subject)
 admin.site.register(Room)
 admin.site.register(DayTime)
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'teacher', 'course')
--- brimstone/dashboard/views.py
+++ brimstone/dashboard/views.py
@@ -1,13 +1,15 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
-from courses.models import Course, Lesson, Subject, Room, DayTime
+from courses.models import Lesson 
 
 
 @login_required
 def dashboard(request):
     if request.user.is_staff or request.user.is_superuser:
-        return render(request, 'dashboard.html', {})
+        lessons=Lesson.objects.filter(subject__teacher=request.user)
+
+        return render(request, 'dashboard.html', {'lessons':lessons})
     else:
         messages.success(request, 'You are not a staff member!')
         return redirect('home')
