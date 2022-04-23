7865e97e7c308bcb6cf6561b4f488db4ef819cb1
ryonistic@protonmail.com <ryonistic@protonmail.com>
2022-04-23 10:59:53 +0530

    Added Document verification and application status check

--- brimstone/admissions/models.py
+++ brimstone/admissions/models.py
@@ -77,7 +77,7 @@ class Document(models.Model):
     address_proof = models.FileField()
     undertaking = models.FileField()
     verified = models.BooleanField(default=False)
-    application = models.OneToOneField(Admission, null=True,blank=True, on_delete=models.CASCADE)
+    application = models.OneToOneField(Admission, null=True, on_delete=models.CASCADE)
    
     def __str__(self):
         return str(self.application.admission_id)
